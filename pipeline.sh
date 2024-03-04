beb517ed4afc26be7f1eed51b3cd286b678b0e5d
Ricky <rickycurry@gmail.com>
2024-03-04 15:49:51 -0800

    Add git automation to pipeline.sh

--- pipeline.sh
+++ pipeline.sh
@@ -7,3 +7,10 @@ if [ $? -ne 0 ] ; then
 fi
 python3 -u process_reviews.py
 python3 -u labels.py
+
+cd ..
+git add ./data/reviews.json
+git add ./data/labels.json
+git commit -m "Data as of $(date)"
+git push origin master
+cd ./pipeline
